5ec6ed99ea2c74353a90de5e73e88960225db1c2
Waasif Hafeez <wasifhafeez26@gmail.com>
2024-08-05 16:12:06 +0500

    completed UniqueValues

--- code/02_unique_values/02_unique_values.js
+++ code/02_unique_values/02_unique_values.js
@@ -30,5 +30,28 @@ const menu = [
         category: 'dinner',
     },
 ]
+// new Set() - es6 introduced us to a new data structure that will return us unique values
 
+  const categories = menu.map((item) => item.category)
+  console.log(categories);
   
+//   const categories1 =new Set( menu.map((item) => item.category))        // Set - returns unique values
+//   console.log(categories1);
+  // result => Set { 0: 'breakfast', 1: 'lunch', 2: 'dinner' }
+
+  // now problem is Set return us object - so now we have to convert it into a n array through spread operator
+
+  const categories1 =['all', ...new Set( menu.map((item) => item.category))]        // Set - returns unique values
+  console.log(categories1);
+
+//  one way 
+
+//   const categoriesArray = categories1.map(item => `<h3>category: ${item}</h3>`)
+
+//   const result = document.querySelector('#result');
+//   result.innerHTML = categoriesArray.join("")
+
+// other way
+
+const result = document.querySelector('#result');
+result.innerHTML = categories1.map(item => `<button>${item}</button>`).join("");        // join("") is added bcs map is returning array so we converted array to string with the help of join method
