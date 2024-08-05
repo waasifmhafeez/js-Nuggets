ea95a1154f766638009eea3822dbdd6de2a38009
Waasif Hafeez <wasifhafeez26@gmail.com>
2024-08-05 15:46:11 +0500

    file structure updated

---
+++ code/02_unique_values/02_unique_values.js
@@ -0,0 +1,34 @@
+// ********* Unique Values **********
+
+// map - get all instances
+// new Set - narrow down
+// ['all',...] - turn it back to array
+
+const menu = [
+    {
+        name: 'pancakes',
+        category: 'breakfast',
+    },
+    {
+        name: 'burger',
+        category: 'lunch',
+    },
+    {
+        name: 'steak',
+        category: 'dinner',
+    },
+    {
+        name: 'bacon',
+        category: 'breakfast',
+    },
+    {
+        name: 'eggs',
+        category: 'breakfast',
+    },
+    {
+        name: 'pasta',
+        category: 'dinner',
+    },
+]
+
+  
