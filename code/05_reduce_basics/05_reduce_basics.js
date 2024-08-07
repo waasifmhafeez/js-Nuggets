b1559b125a8c3e4019877f928cb25456164c661a
Waasif Hafeez <wasifhafeez26@gmail.com>
2024-08-07 23:30:58 +0500

    completed reduce_basics

---
+++ code/05_reduce_basics/05_reduce_basics.js
@@ -0,0 +1,23 @@
+
+
+// Reduce
+// iterates, callback function
+// reduces to a single value - number, array, object
+// 1st parameter ('acc') - total of all calculations
+// 2nd parameter ('curr') - current iteration/ value
+
+const staff = [
+    { name: 'bob', age: 20, position: 'developer', salary: 100},
+    { name: 'peter', age: 25, position: 'designer', salary: 300},
+    { name: 'susy', age: 30, position: 'the boss', salary: 400},
+    { name: 'anna', age: 35, position: 'intern', salary: 90},
+]
+
+const dailyTotal = staff.reduce((total, person) => {
+    console.log(total);
+    console.log(person.salary);
+    total += person.salary
+    return total
+},10)
+
+console.log(dailyTotal);
