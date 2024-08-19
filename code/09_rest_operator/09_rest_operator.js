9b88ea5f4f9c8e510c585912b5fe37d98c5b0445
Waasif Hafeez <wasifhafeez26@gmail.com>
2024-08-19 23:48:58 +0500

    working on rest_operator

---
+++ code/09_rest_operator/09_rest_operator.js
@@ -0,0 +1,14 @@
+// REST Operator
+// gathers/ collects items
+// destructuring, functions
+// placement important, careful with the same name
+// rest when declare function, spread when invoke the function
+
+// array
+const fruits = ['apple', 'orange', 'lemon', 'banana'];
+const [first] = fruits;
+
+// objects
+const person = { name: 'john', lastName: 'smith', job: 'developer'};
+const {name} = person;
+console.log(name);
