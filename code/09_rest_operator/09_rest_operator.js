c02f818c028771fc726b620d11fbaa85eb865c64
Waasif Hafeez <wasifhafeez26@gmail.com>
2024-08-20 00:43:46 +0500

    completed rest_operator

--- code/09_rest_operator/09_rest_operator.js
+++ code/09_rest_operator/09_rest_operator.js
@@ -5,10 +5,52 @@
 // rest when declare function, spread when invoke the function
 
 // array
-const fruits = ['apple', 'orange', 'lemon', 'banana'];
-const [first] = fruits;
+const fruits = ['apple', 'orange', 'lemon', 'banana', 'pear'];
+
+// const [first] = fruits;
+
+// now do it with rest operation, it'll assign first element of array to 'first' , and returns the rest of the elements into a array called 'rest
+const [first, second, ...restOfTheFruits] = fruits     // rest operator will collect all the remaining values of the array which we didnt destructred with name
+
+
+console.log(first, second, restOfTheFruits);     // result => [ 'apple', 'orange', [ 'lemon', 'banana', 'pear' ] ]
+
+const specificFruit = restOfTheFruits.find(fruit => fruit === 'lemon')
+
+console.log(specificFruit );
+
+
 
 // objects
-const person = { name: 'john', lastName: 'smith', job: 'developer'};
-const {name} = person;
-console.log(name);
+const person = { name: 'john', lastName: 'smith', job: 'developer', age: 22, religon: 'islam'};
+
+// const {name} = person;
+// console.log(name);
+
+const {name, lastName, ...restOfObject} = person;
+
+console.log(name, lastName, restOfObject);      // result => [ 'john', 'smith', { job: 'developer', age: 22, religon: 'islam' } ]
+// in case of object it will return rest of the properties in to a object
+
+// functions
+
+const getAverage = (name, ...scores) => {
+    console.log(name)
+    console.log(scores)
+
+    // const avg = scores.reduce((total, item) => {
+    //     total += item;
+    //     return total;
+    // },0) / scores.length;
+
+    const avg = scores.reduce((total, item) => {return total += item},0) / scores.length;       // one liner logic of above code
+
+    console.log(avg)
+}
+// getAverage('wasif', 22,23,23,21,2,32)
+
+// if you have put values from an array
+const testScores =  [23,56,47,73];
+
+getAverage(person.name, ...testScores)
+ 
